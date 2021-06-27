8b578b11c243af98197ad2e23fa7e06089e7b612
Dipika2020 <64186626+Dipika2020@users.noreply.github.com>
2021-06-27 19:42:27 +0530

    Add files via upload

---
+++ Info3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info3() {
+  return (
+    <div class="note">
+      <h1>Python and Data Visualization</h1>
+      <p>Foundation of Data Visualization and Hands-on project</p>
+    </div>
+  );
+}
+
+export default Info3;
